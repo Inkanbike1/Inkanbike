f107e21cc76b338851e6ac8434213348ffdf79bc
angel <aolmos@inkanbike.cl>
2016-05-10 22:05:22 -0300

    test

--- app/src/main/java/cl/ozcc/inkanbike/MainActivity.java
+++ app/src/main/java/cl/ozcc/inkanbike/MainActivity.java
@@ -22,6 +22,10 @@ import cl.ozcc.inkanbike.objects.Valid;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static Valid validObj = new Valid();
+    static User user = new User();
+    private  static MessageUI msjUi;
+    private static Intent inkanAct;
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -31,16 +35,10 @@ public class MainActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
-    private  static MessageUI msjUi;
-    private final static  Valid validObj = new Valid();
-    private static Intent inkanAct;
-    static User user= new User();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }else{
             startActivity(inkanAct);
+            //Cambio realizado :D
             finish();
         }
         // Create the adapter that will return a fragment for each of the three
@@ -80,28 +79,7 @@ public class MainActivity extends AppCompatActivity {
     public void ChangeRight(View v){
         mViewPager.arrowScroll(View.FOCUS_RIGHT);
     }
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-        @Override
-        public int getCount() {
-            // Show total pages.
-            return 2;
-        }
-    }
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -128,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
                                  Bundle savedInstanceState) {
             final View rootView;
             final Context ctx;
-            if(getArguments().getInt(ARG_SECTION_NUMBER) == 1){
+            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
                 rootView = inflater.inflate(R.layout.fragment_main_one, container, false);
                 ctx = rootView.getContext();
 
@@ -136,47 +114,47 @@ public class MainActivity extends AppCompatActivity {
                 final EditText pass = (EditText) rootView.findViewById(R.id.passIng);
 
                 Button login = (Button) rootView.findViewById(R.id.btnIngresar);
-                    login.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            int usrId = new Valid().ValidLogin(email.getText().toString(), pass.getText().toString(), getContext());
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int usrId = new Valid().ValidLogin(email.getText().toString(), pass.getText().toString(), getContext());
 
-                            if(usrId != 0){
-                                Intent login = new Intent(ctx, InkanActivity.class);
-                                ctx.startActivity(login);
+                        if (usrId != 0) {
+                            Intent login = new Intent(ctx, InkanActivity.class);
+                            ctx.startActivity(login);
 
-                                new DataHelper(ctx).setLogin();
-                                user.findUserFromServer(usrId).save(ctx);
-                                getActivity().finish();
+                            new DataHelper(ctx).setLogin();
+                            user.findUserFromServer(usrId).save(ctx);
+                            getActivity().finish();
 
-                            }else{
-                                msjUi.InflateDialog(ctx.getString(R.string.login_title),ctx.getString(R.string.LOGIN_FAIL));
-                            }
+                        } else {
+                            msjUi.InflateDialog(ctx.getString(R.string.login_title), ctx.getString(R.string.LOGIN_FAIL));
                         }
-                    });
-            }else{
+                    }
+                });
+            } else {
                 rootView = inflater.inflate(R.layout.fragment_main_two, container, false);
                 ctx = rootView.getContext();
-                final EditText nameReg   = (EditText) rootView.findViewById(R.id.newName);
-                final EditText emailReg  = (EditText) rootView.findViewById(R.id.newEmail);
-                final EditText passReg   = (EditText) rootView.findViewById(R.id.newPass);
-                final EditText pass2Reg  = (EditText) rootView.findViewById(R.id.newPass2);
+                final EditText nameReg = (EditText) rootView.findViewById(R.id.newName);
+                final EditText emailReg = (EditText) rootView.findViewById(R.id.newEmail);
+                final EditText passReg = (EditText) rootView.findViewById(R.id.newPass);
+                final EditText pass2Reg = (EditText) rootView.findViewById(R.id.newPass2);
 
                 emailReg.setOnFocusChangeListener(new View.OnFocusChangeListener() {
                     @Override
                     public void onFocusChange(View v, boolean hasFocus) {
                         if (!hasFocus) {
-                            if (!validObj.ValidEmail(emailReg.getText().toString())){
+                            if (!validObj.ValidEmail(emailReg.getText().toString())) {
                                 Toast.makeText(getContext(), getString(R.string.EMAIL_REGISTERED), Toast.LENGTH_LONG).show();
                             }
                         }
                     }
                 });
                 Button register = (Button) rootView.findViewById(R.id.btnRegistroIng);
-                    register.setOnClickListener(new View.OnClickListener(){
-                        @Override
-                        public void onClick (View v){
-                            User.email = emailReg.getText().toString();
+                register.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        User.email = emailReg.getText().toString();
                         if (passReg.getText().toString().equals(pass2Reg.getText().toString())) {
                             if (validObj.ValidEmail(User.email)) {
 
@@ -199,9 +177,33 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(getContext(), getString(R.string.PASS_NOT_EQUALS), Toast.LENGTH_LONG).show();
                         }
                     }
-                    });
+                });
             }
             return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show total pages.
+            return 2;
         }
     }
 }
