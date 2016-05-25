cce20d46f31ea73a762107874dbed7953b42190d
angel <aolmos@inkanbike.cl>
2016-05-24 20:15:48 -0400

    test

--- app/src/main/java/cl/ozcc/inkanbike/SosActivity.java
+++ app/src/main/java/cl/ozcc/inkanbike/SosActivity.java
@@ -1,11 +1,13 @@
 package cl.ozcc.inkanbike;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -20,16 +22,20 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import cl.ozcc.inkanbike.objects.MessageUI;
 import cl.ozcc.inkanbike.objects.Sos;
+import cl.ozcc.inkanbike.objects.User;
 
 public class SosActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
 
+    final Handler handler = new Handler();
     GoogleApiClient mGoogleApiClient;
     SupportMapFragment SupportMap;
     GoogleMap Gmap;
     Sos sos = new Sos();
+    Boolean send = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +53,9 @@ public class SosActivity extends FragmentActivity implements OnMapReadyCallback,
         TextView distance = (TextView) findViewById(R.id.sosDistanRec);
         TextView message = (TextView) findViewById(R.id.sosMsjRec);
 
-        type.setText(sos.getType());
+        String[] sosName = getResources().getStringArray(R.array.SOS_ARRAY);
+
+        type.setText(sosName[sos.getType()]);
         distance.setText(sos.getDistance() + " Mts.");
         message.setText(sos.getMessage());
 
@@ -55,13 +63,40 @@ public class SosActivity extends FragmentActivity implements OnMapReadyCallback,
         respSos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final AlertDialog alert = new MessageUI(SosActivity.this)
+                        .GetAlertDialog(
+                                getApplicationContext().getResources().getString(R.string.SEND_SOS_USERS),
+                                getApplicationContext().getResources().getString(R.string.SOS_RESP_OK)
+                        );
+                alert.setButton(DialogInterface.BUTTON_POSITIVE, "Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        send = true;
+                    }
+                });
+                alert.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        send = false;
+                    }
+                });
+                alert.show();
 
-                Toast.makeText(getApplicationContext(), "HOLI", Toast.LENGTH_SHORT).show();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (alert.isShowing()) {
+                            alert.dismiss();
+                        }
+                        if (send) {
+                            new User().responseSos(sos.getSosId());
+                        }
+                    }
+                }, 3000);
+
             }
         });
-
     }
-
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         try {
@@ -81,14 +116,10 @@ public class SosActivity extends FragmentActivity implements OnMapReadyCallback,
 
     @Override
     public void onConnected(Bundle bundle) {
-
     }
-
     @Override
     public void onConnectionSuspended(int i) {
-
     }
-
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
     }
